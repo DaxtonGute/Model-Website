af13d8d515d54ea90d8c9a9bd5d9ebed04a1a668
DaxGute <36718376+DaxGute@users.noreply.github.com>
2020-09-27 20:05:47 -0700

    save #1
    I done for the night

--- script.js
+++ script.js
@@ -10,7 +10,37 @@ function toggleMenuSwitch() {
     document.getElementById('DDM').style.display = "block";
     document.getElementById('menuSwitch').innerHTML = "☓";
     toggled = true;
-
   }
+}
 
+
+
+// try to get animation to work!!! please!
+function slideUp(){
+ var elem = document.getElementById("submenu");
+ var pos = 0;
+ var id = setInterval(frame, 1);
+ function frame() {
+   if (pos == 350) {
+     clearInterval(id);
+   } else {
+     pos++;
+     elem.style.top = pos + 'px';
+   }
+ }
+}
+
+function slideDown(){
+  var elem = document.getElementById("menuSwitch");
+  var pos = 360;
+  var id = setInterval(frame, 10);
+  function frame() {
+   if (pos == 0) {
+     clearInterval(id);
+   } else {
+     pos--;
+     elem.style.color = "rgb(0,0,"+(pos/2)+");";
+     elem.style.top = pos + 'px';
+   }
+  }
 }
